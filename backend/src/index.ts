8e10d7e370720d7492ff899aba20d7dcb002c6d3
dmmarodin <douglasmarodin@gmail.com>
2024-02-01 00:08:53 -0300

    [ADD] Inicia servidor backend

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+require('dotenv').config();
+import express from 'express';
+import { router } from './routes';
+
+const app = express();
+
+
+app.use(router);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Executando backend na porta ${process.env.PORT}`);
+});
